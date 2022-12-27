5327ba1ea9e3f329669561968b0aa0a9000c56ad
Kujima <bertaux.martin@gmail.com>
2022-12-27 13:47:26 +0100

    init day7

---
+++ AdventOfCode/Algo/AlgoDay7.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode.Algo;
+
+using System;
+
+public class AlgoDay7 : IAlgoDay
+{
+    private readonly string DayNumber;
+
+    public AlgoDay7()
+    {
+        this.DayNumber = typeof(AlgoDay7).Name.Last().ToString();
+    }
+
+    public void Solve1() { }
+
+    public void Solve2() { }
+}
